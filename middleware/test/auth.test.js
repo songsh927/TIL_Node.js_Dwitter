e4ab697b4b51e2719136edd040a9937c23c5e766
송승현 <songsh96927@naver.com>
2022-07-12 11:46:24 +0900

    22.07.16

---
+++ middleware/test/auth.test.js
@@ -0,0 +1,23 @@
+import httpMocks from 'node-mocks-http';
+import { isAuth } from '../auth';
+
+describe('Auth Middleware', () => {
+    it('returns 401 for the request without Authorization header',() => {
+        //given
+        const request = httpMocks.createRequest({
+            method : 'GET',
+            url: '/tweets',
+        });
+        const response = httpMocks.createResponse();
+        const next = jest.fn();
+
+        //when
+        isAuth(request, response, next);
+
+        //then
+        expect(response.statusCode).toBe(401);
+        expect(response._getJSONData().message).toBe('Authentication Error');
+        expect(next).not.toBeCalled();
+
+    })
+})
